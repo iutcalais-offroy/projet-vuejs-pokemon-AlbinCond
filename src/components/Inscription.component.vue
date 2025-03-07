704353e81aad89148f30af17d3c367d7f00b590f
AlbinCond <condettealbin@gmail.com>
2025-03-07 16:15:32 +0100

    mise en composant de la connection

---
+++ src/components/Inscription.component.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="form-container">
+      <n-form @submit.prevent="inscrire">
+        <n-form-item label="Username">
+          <n-input v-model:value="username" placeholder="Entrez votre nom d'utilisateur" />
+        </n-form-item>
+        <n-form-item label="Password">
+          <n-input v-model:value="password" placeholder="Entrez votre mot de passe" type="password" />
+        </n-form-item>
+        <n-form-item label="Reenter Password">
+          <n-input v-model:value="password2" placeholder="Confirmez votre mot de passe" type="password" />
+        </n-form-item>
+        <n-form-item>
+          <n-button type="primary" native-type="submit">S'inscrire</n-button>
+        </n-form-item>
+      </n-form>
+      <p class="link">Déjà inscrit ? <span @click="$emit('toggleSignin')">Se connecter</span></p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  const username = ref('');
+  const password = ref('');
+  const password2 = ref('');
+  const router = useRouter();
+  
+  const inscrire = () => {
+    console.log('Inscription avec', username.value, password.value, password2.value);
+    // Logique d'inscription
+    router.push('/login');
+  };
+  </script>
+  
+  <style scoped>
+  .form-container {
+    width: 100%;
+  }
+  .link {
+    text-align: center;
+    margin-top: 10px;
+    color: black;
+  }
+  .link span {
+    color: green;
+    cursor: pointer;
+  }
+  </style>
--- src/pages/LoginForm.page.vue
+++ src/pages/LoginForm.page.vue
@@ -1,86 +1,33 @@
 <template>
-  <div class="container">
-    <n-tabs v-model:value="isSignup" type="line">
-      <n-tab-pane name="true" tab="Connexion">
-        <div class="form-container">
-          <n-form @submit.prevent="connecter">
-            <n-form-item label="Email">
-              <n-input v-model:value="email" placeholder="Entrez votre email" type="email" />
-            </n-form-item>
-            <n-form-item label="Mot de passe">
-              <n-input v-model:value="password" placeholder="Entrez votre mot de passe" type="password" />
-            </n-form-item>
-            <n-form-item>
-              <n-button type="primary" native-type="submit">Se connecter</n-button>
-            </n-form-item>
-          </n-form>
-          <p class="link">Pas de compte ? <span @click="isSignup = false">S'inscrire</span></p>
-        </div>
+  <n-card>
+    <n-tabs
+      class="card-tabs"
+      v-model:value="activeTab"
+      size="large"
+      animated
+      pane-wrapper-style="margin: 0 -4px"
+      pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+    >
+      <n-tab-pane name="connexion" tab="Connexion">
+        <ConnexionComponent @toggleSignup="activeTab = 'signup'" />
       </n-tab-pane>
-      <n-tab-pane name="false" tab="Inscription">
-        <div class="form-container">
-          <n-form @submit.prevent="inscrire">
-            <n-form-item label="Email">
-              <n-input v-model:value="email" placeholder="Entrez votre email" type="email" />
-            </n-form-item>
-            <n-form-item label="Mot de passe">
-              <n-input v-model:value="password" placeholder="Entrez votre mot de passe" type="password" />
-            </n-form-item>
-            <n-form-item label="Confirmer le mot de passe">
-              <n-input v-model:value="password2" placeholder="Confirmez votre mot de passe" type="password" />
-            </n-form-item>
-            <n-form-item>
-              <n-button type="primary" native-type="submit">S'inscrire</n-button>
-            </n-form-item>
-          </n-form>
-          <p class="link">Déjà inscrit ? <span @click="isSignup = true">Se connecter</span></p>
-        </div>
+      <n-tab-pane name="inscription" tab="Inscription">
+        <InscriptionComponent @toggleSignin="activeTab = 'signin'" />
       </n-tab-pane>
     </n-tabs>
-  </div>
+  </n-card>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { NForm, NFormItem, NInput, NButton, NTabs, NTabPane } from 'naive-ui';
+import ConnexionComponent from '../components/Connexion.component.vue';
+import InscriptionComponent from '../components/Inscription.component.vue';
 
-const isSignup = ref(true);
-const email = ref('');
-const password = ref('');
-const password2 = ref('');
-const router = useRouter();
-
-const connecter = () => {
-  console.log('Connexion avec', email.value, password.value);
-  // Logique de connexion
-  router.push('/deckbuilder');
-};
-
-const inscrire = () => {
-  console.log('Inscription avec', email.value, password.value, password2.value);
-  // Logique d'inscription
-  router.push('/login');
-};
+const activeTab = ref('signin');
 </script>
 
 <style scoped>
-.container {
-  width: 300px;
-  padding: 20px;
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  background: white;
-}
-
-.link {
-  text-align: center;
-  margin-top: 10px;
-  color: black;
-}
-
-.link span {
-  color: green;
-  cursor: pointer;
+.card-tabs .n-tabs-nav--bar-type {
+  padding-left: 4px;
 }
 </style>
